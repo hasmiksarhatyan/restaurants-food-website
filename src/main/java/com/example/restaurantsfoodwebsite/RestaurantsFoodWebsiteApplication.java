20603932ece88e138d133fc5e98b7d2bfb096c9a
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-09-29 20:42:41 +0400

    Initial commit

---
+++ src/main/java/com/example/restaurantsfoodwebsite/RestaurantsFoodWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.restaurantsfoodwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantsFoodWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantsFoodWebsiteApplication.class, args);
+	}
+
+}
